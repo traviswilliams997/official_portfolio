c58e9c7992ac47ceebd3ec0c05ef4541952115fa
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-03-04 11:28:40 -0500

    Add content to top left corner

--- src/components/TopLeft.jsx
+++ src/components/TopLeft.jsx
@@ -1,16 +1,36 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 
 const TopLeft = () => {
   return (
-    <Box bgcolor="#3AAFA9" flex={10} height="400px">
-      <Box>
+    <Box
+      bgcolor="#3AAFA9"
+      flex={10}
+      height="400px"
+      display="flex"
+      justifyContent="center"
+    >
+      <Stack>
         <Typography variant="h2" fontWeight={700} color="#FEFFFF">
           Software
         </Typography>
-        <Typography variant="h3" fontWeight={700} color="#FEFFFF">
-          Engineer
+        <Typography variant="h2" fontWeight={700} color="#FEFFFF">
+          Engineer.
         </Typography>
-      </Box>
+        <Typography variant="h5" fontWeight={400} color="#17252A" width="500px">
+          Focused on solving real problems that add measurable gains to the
+          bottom line.
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={400}
+          color="#2A7A78"
+          width="350px"
+          mt="10px"
+        >
+          I achieve this by crafting: modular, well tested, maintainable and
+          scalelabe software systems.
+        </Typography>
+      </Stack>
     </Box>
   )
 }
