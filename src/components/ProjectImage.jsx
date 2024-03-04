5714661eafd3a096f8f02c6118a932c377780ab0
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-03-04 17:01:07 -0500

    Edit image component to display image properly

--- src/components/ProjectImage.jsx
+++ src/components/ProjectImage.jsx
@@ -1,9 +1,15 @@
 import { Box } from '@mui/material'
 
-const ProjectImage = () => {
+const ProjectImage = ({ imgUrl }) => {
   return (
-    <Box bgcolor="#FEFFF" border="10px solid  #DEF2F1" flex={1} height="400px">
-      Project Image
+    <Box
+      display="flex"
+      bgcolor="#FEFFF"
+      border="20px solid  #DEF2F1"
+      flex={1}
+      height="400px"
+    >
+      <img className="project" src={imgUrl} width="100%"></img>
     </Box>
   )
 }
