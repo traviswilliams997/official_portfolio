862684bea37b37f91afab465a1c82f7219e15600
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-03-06 18:10:56 -0500

    Add social profiles to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,15 @@ import {
   useMediaQuery,
   Button,
   TextField,
+  Divider,
+  Link,
 } from '@mui/material'
+import { EditOutlined } from '@mui/icons-material'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 import emailjs from '@emailjs/browser'
 import { useRef } from 'react'
+import FlexBetween from './FlexBetween'
 
 const Contact = () => {
   const isNonMobile = useMediaQuery('(min-width:600px)')
@@ -63,9 +67,70 @@ const Contact = () => {
     >
       <Typography variant="h3" color="#3AAFA9">
         CONTACT ME
-      </Typography>
-      <Box mt="50px">
-        {' '}
+      </Typography>{' '}
+      <Box p="1rem 0" mt="20px" width={'22vw'}>
+        <Typography variant="h4" color={'#3AAFA9'} fontWeight="500" mb="1rem">
+          Socials
+        </Typography>
+
+        <FlexBetween gap="1rem" mb="1rem">
+          <FlexBetween gap="1rem">
+            <img
+              height="50px"
+              width="50px"
+              src="https://store-images.s-microsoft.com/image/apps.31120.9007199266245564.44dc7699-748d-4c34-ba5e-d04eb48f7960.bc4172bd-63f0-455a-9acd-5457f44e4473"
+              alt="linkedin"
+            />
+            <Link
+              href={'https://linkedin.com/in/travis-travis'}
+              sx={{ textDecoration: 'none' }}
+              color="inherit"
+            >
+              <Typography variant="h5" color={'#DEF2F1'} fontWeight="500">
+                LinkedIn
+              </Typography>
+            </Link>
+          </FlexBetween>
+          <Link
+            href={'https://linkedin.com/in/travis-travis'}
+            sx={{ textDecoration: 'none' }}
+            color="inherit"
+          >
+            <EditOutlined sx={{ color: '#DEF2F1' }} />
+          </Link>
+        </FlexBetween>
+        <FlexBetween gap="1rem" mb="1rem">
+          <FlexBetween gap="1rem">
+            <img
+              height="50px"
+              width="50px"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png"
+              alt="twitter"
+            />
+            <Link
+              href={'https://twitter.com/travis_87x'}
+              sx={{ textDecoration: 'none' }}
+              color="inherit"
+            >
+              <Typography variant="h5" color={'#DEF2F1'} fontWeight="500">
+                Twitter
+              </Typography>
+            </Link>
+          </FlexBetween>
+          <Link
+            href={'https://twitter.com/travis_87x'}
+            sx={{ textDecoration: 'none' }}
+            color="inherit"
+          >
+            <EditOutlined sx={{ color: '#DEF2F1' }} />
+          </Link>
+        </FlexBetween>
+      </Box>
+      <Divider color="white" />
+      <Box mt="20px" width={'22vw'}>
+        <Typography variant="h4" color={'#3AAFA9'} fontWeight="500">
+          Email
+        </Typography>{' '}
         <Formik
           onSubmit={handleFormSubmit}
           initialValues={initialValues}
