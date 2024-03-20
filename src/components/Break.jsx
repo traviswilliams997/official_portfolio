13cce6b03b42bfce2837590801ba15bfa2a879a7
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-03-20 11:08:32 -0500

    Edit projects title section

--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -3,6 +3,7 @@ const Break = () => {
   const theme = useTheme()
   const dark = theme.palette.primary.dark
   const light = theme.palette.primary.light
+  const secondary = theme.palette.secondary.light
 
   return (
     <Box
@@ -17,7 +18,13 @@ const Break = () => {
       <Typography variant={'h2'} fontWeight={500} color={light} id="projects">
         PROJECTS
       </Typography>
-      <Typography variant={'h6'} color={light} id="projects">
+      <Typography
+        variant={'h7'}
+        color={secondary}
+        id="projects"
+        pl={'20px'}
+        pr={'20px'}
+      >
         *Hosted on free servers that take a minute to start up
       </Typography>
     </Box>
