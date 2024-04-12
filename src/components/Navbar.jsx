cf4be1875163971ead488bcf6e3f68750e961c5d
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-04-11 20:39:24 -0500

    Edit navbar to use env variable for resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -175,7 +175,7 @@ const Navbar = () => {
                         },
                       }}
                       fontWeight={700}
-                      href="https://drive.google.com/file/d/1CjuN3-NKk2I5f4pZJwZplHcClsxc2bNe/view?usp=sharing"
+                      href={import.meta.env.VITE_RESUME_LINK}
                     >
                       Resume
                     </Link>
